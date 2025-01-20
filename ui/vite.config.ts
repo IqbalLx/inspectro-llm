4a0feff536df81107936832dfa6caee75dcefc6c
IqbalLx <iqbal19600@gmail.com>
2025-01-20 23:52:07 +0700

    chore deps ui

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,9 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, PluginOption } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import Unfonts from "unplugin-fonts/vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    TanStackRouterVite(),
+    Unfonts({
+      custom: {
+        families: [
+          {
+            name: "Geist Sans",
+            src: "./src/assets/fonts/geist/*.woff2",
+          },
+        ],
+      },
+    }) as PluginOption,
+  ],
   build: {
     outDir: "../server/_ui/build",
     emptyOutDir: true,
